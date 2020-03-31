cdc4bd3d3e9f6b36b42f46213af234ec301148ad
chic-caller-7520 <arbrown110@gmail.com>
2020-03-31 23:40:07 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -11,3 +11,13 @@ def my_collect(array)
   end
   collection
 end
+
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
+end
